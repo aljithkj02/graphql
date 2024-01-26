d99c4507b6fa0a98bf06dad5d4ba962ef4d4b6f3
Aljith KJ <kwdeathlok@gmail.com>
2024-01-26 10:09:13 +0530

    Create the schema for the type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import { typeDefs } from './schema'
 
-const server = new ApolloServer({
-    // typeDefs
-    // resolvers
-})
-
-const { url } = await startStandaloneServer(server, {
-    listen: {
-        port: 4000
-    }
-})
-
-console.log('Server ready at port: 4000')
+(async () => { 
+    const server = new ApolloServer({
+        // typeDefs -- Definitions of types of data 
+        // resolvers
+        typeDefs
+    })
+    
+    const { url } = await startStandaloneServer(server, {
+        listen: {
+            port: 4000
+        }
+    })
+    
+    console.log('Server ready at port: 4000')
+})()
