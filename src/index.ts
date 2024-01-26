b8a02ee348912afac110e10f3103a825430d5bc9
Aljith KJ <kwdeathlok@gmail.com>
2024-01-26 10:37:29 +0530

    Add resolvers and run the first query

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { typeDefs } from './schema'
+import { authors, games, reviews } from './_db'
+
+const resolvers = {
+    Query: {
+        games() {
+            return games;
+        },
+        reviews() {
+            return reviews;
+        },
+        authors() {
+            return authors;
+        }
+    }
+};
 
 (async () => { 
     const server = new ApolloServer({
         // typeDefs -- Definitions of types of data 
         // resolvers
-        typeDefs
+        typeDefs,
+        resolvers
     })
     
     const { url } = await startStandaloneServer(server, {
