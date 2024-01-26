ec6d1bc5d121ec7e8312cd7cfe9f0ae1c689e63e
Aljith KJ <kwdeathlok@gmail.com>
2024-01-26 10:37:00 +0530

    Add in memory db data

---
+++ src/_db.ts
@@ -0,0 +1,118 @@
+export const games = [
+  {
+    id: "1",
+    title: "Assassin's Creed Valhalla",
+    platform: ["PC", "PlayStation", "Xbox"],
+  },
+  {
+    id: "2",
+    title: "The Legend of Zelda: Breath of the Wild",
+    platform: ["Nintendo Switch"],
+  },
+  {
+    id: "3",
+    title: "FIFA 22",
+    platform: ["PlayStation", "Xbox"],
+  },
+  {
+    id: "4",
+    title: "Cyberpunk 2077",
+    platform: ["PC", "PlayStation", "Xbox"],
+  },
+  {
+    id: "5",
+    title: "Super Mario Odyssey",
+    platform: ["Nintendo Switch"],
+  },
+];
+
+export const authors = [
+  {
+    id: "101",
+    name: "John Doe",
+    verified: true,
+  },
+  {
+    id: "102",
+    name: "Jane Smith",
+    verified: false,
+  },
+  {
+    id: "103",
+    name: "Bob Johnson",
+    verified: true,
+  },
+];
+
+export const reviews = [
+  {
+    id: "201",
+    rating: 4,
+    content: "Enjoyed the open-world exploration!",
+    authorId: "101",
+    gameId: "1",
+  },
+  {
+    id: "202",
+    rating: 5,
+    content: "Amazing game, a must-play!",
+    authorId: "102",
+    gameId: "2",
+  },
+  {
+    id: "203",
+    rating: 3,
+    content: "Good sports simulation, but needs improvements.",
+    authorId: "103",
+    gameId: "3",
+  },
+  {
+    id: "204",
+    rating: 2,
+    content: "Disappointing performance on my system.",
+    authorId: "101",
+    gameId: "4",
+  },
+  {
+    id: "205",
+    rating: 4,
+    content: "Fun and challenging gameplay!",
+    authorId: "102",
+    gameId: "5",
+  },
+  {
+    id: "206",
+    rating: 4,
+    content: "Great storyline and character development.",
+    authorId: "103",
+    gameId: "1",
+  },
+  {
+    id: "207",
+    rating: 5,
+    content: "Innovative game design, love the puzzles!",
+    authorId: "101",
+    gameId: "2",
+  },
+  {
+    id: "208",
+    rating: 3,
+    content: "Decent graphics, but lacking in certain areas.",
+    authorId: "102",
+    gameId: "3",
+  },
+  {
+    id: "209",
+    rating: 4,
+    content: "Despite the bugs, a memorable gaming experience.",
+    authorId: "103",
+    gameId: "4",
+  },
+  {
+    id: "210",
+    rating: 5,
+    content: "Nostalgic and creative, a true masterpiece!",
+    authorId: "101",
+    gameId: "5",
+  },
+];
