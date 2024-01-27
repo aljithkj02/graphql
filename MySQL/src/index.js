4b6a097b4c8642b9784344505e7836aa3b99fc23
Aljith KJ <kwdeathlok@gmail.com>
2024-01-27 14:47:01 +0530

    Integrate the graphql server with express

--- MySQL/src/index.js
+++ MySQL/src/index.js
@@ -1,14 +1,18 @@
 import { ApolloServer } from '@apollo/server'
+import { ApolloServer as ApolloServerExpress } from 'apollo-server-express'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import express from 'express'
 import { typeDefs } from './graph/typeDef.js'
 import { resolvers } from './graph/resolvers.js'
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+
 
 const startServer = async () => {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+
     const { url } = await startStandaloneServer(server, {
         listen: {
             port: 3000
@@ -17,4 +21,23 @@ const startServer = async () => {
     console.log("Server setup ", url)
 }
 
-startServer();
+const startExpressServer = async () => {
+    const server = new ApolloServerExpress({
+        typeDefs,
+        resolvers
+    })
+
+    const app = express();
+    await server.start();
+
+    server.applyMiddleware({ app });
+    
+    app.listen(4000, () => {
+        console.log("Server started on: localhost:4000");
+        console.log(server.graphqlPath);
+    })
+}
+
+
+// startServer();
+startExpressServer();
