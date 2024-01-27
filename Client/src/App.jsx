387e53ae115db45a94c092de998fd710524fb565
Aljith KJ <kwdeathlok@gmail.com>
2024-01-27 09:29:28 +0530

    Add basic fetching using appolo client

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,33 @@
 import { Box, Typography } from '@mui/material'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+import { useEffect } from 'react'
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache()
+})
 
 function App() {
+
+  useEffect(() => {
+    setup();
+  }, [])
+
+  const setup = async () => {
+    const res = await client.query({
+      query: gql`
+        query GetLocations {
+          locations {
+            id
+            name
+            description
+            photo
+          }
+        }
+      `
+    })
+    console.log(res);
+  }
 
   return (
     <Box>
