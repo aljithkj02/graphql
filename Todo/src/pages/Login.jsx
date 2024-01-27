443416aa07df0f81b37e54023154306236fe8354
Aljith KJ <kwdeathlok@gmail.com>
2024-01-27 21:22:35 +0530

    Add mutation for login user

--- Todo/src/pages/Login.jsx
+++ Todo/src/pages/Login.jsx
@@ -1,25 +1,59 @@
 import { Box, Button, Input, Typography } from '@mui/material'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useMutation } from '@apollo/client'
+import { LOGIN_USER } from '../utils/mutations'
 
 export const Login = () => {
   const [showPass, setShowPass] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: ''
+  })
+  const [ loginUser , { loading, error }] = useMutation(LOGIN_USER);
+  const navigate = useNavigate();
 
   const handleShowPass = () => {
     setShowPass(!showPass);
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await loginUser({
+        variables: {
+          ...userInfo
+        }
+      })
+      if(data?.loginUser?.id) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error.message);
+      alert(error.message);
+    }
+  }
+
+  const handleChange = (e) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <Box width="100%" height="100vh" display="flex" alignItems="center" justifyContent="center">
         <Box width="30%">
-          <form>
+          <form onSubmit={handleSubmit}>
             <Box display="flex" flexDirection="column" p={5} bgcolor="ThreeDFace" borderRadius={2}
               gap={2} boxShadow="0 0 10px rgba(0, 0, 0, 0.3)"
             >
               <Typography variant='h4' textAlign="center">Login</Typography>
-              <Input type='email' placeholder='Email' />
+              <Input type='email' placeholder='Email' required name="email" onChange={handleChange}/>
               <Box position="relative">
-                <Input type={showPass ? 'text' : 'password'} placeholder='Password' sx={{ width: "100%"}}/>
+                <Input type={showPass ? 'text' : 'password'} placeholder='Password' sx={{ width: "100%"}} required 
+                  name='password' onChange={handleChange}
+                />
                 <Button variant='outlined' onClick={handleShowPass} sx={{ 
                   fontSize: "11px", position: 'absolute', right: 0, bottom: 5
                 }}>{showPass ? 'Hide' : 'Show'}</Button>
---
+++ Todo/src/utils/mutations.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation LoginUser($email: String!, $password: String!) {
+        loginUser(Input: { email: $email, password: $password }) {
+            id
+            name
+            email
+        }
+    }
+`
