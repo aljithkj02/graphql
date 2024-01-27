cf43d371283e992f2727371ff83d4ae41604aa25
Aljith KJ <kwdeathlok@gmail.com>
2024-01-27 09:09:49 +0530

    Create a basic react app using vite

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <Box>
+      <Typography>Welcome to GraphQL</Typography>
+    </Box>
+  )
+}
+
+export default App
